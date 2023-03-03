4eaf524baea3fc652f84066fa65845c055e3cbf0
kimchiyumi <nhattan0932507137@gmail.com>
2023-03-03 13:06:49 +0700

    123

---
+++ Book/Responsitory/AppDB.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace Example1.Responsitory
+{
+    public class AppDB : DbContext
+    {
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public string ConnectionString { get;}
+        public AppDB() 
+        {
+            ConnectionString = "book.db";        
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite($"Data Source={ConnectionString} ");
+        }
+    }
+}
